dca39365abff62a1c36b96e824faa16e8a34e71b
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-08-12 16:40:43 +1000

    Public API autodocs

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,9 @@ makedocs(
     pages = [
         "Home" => "index.md",
         "SAM and BAM" => "hts-files.md",
+        "API Reference" => [
+            "Public" => "api/public.md"
+        ],
     ],
     authors = replace(join(Pkg.TOML.parsefile("Project.toml")["authors"], ", "), r" <.*?>" => "" ) * ", The BioJulia Organisation, and other contributors."
 )
