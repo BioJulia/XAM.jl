2df146b8fc4578e9631def931bb6441575603b13
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2020-02-02 22:44:55 +1100

    Use FormatSpecimens

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,11 @@ using Test
 using GenomicFeatures
 using XAM
 import BioAlignments: Alignment, AlignmentAnchor, OP_START, OP_MATCH, OP_DELETE
+using FormatSpecimens
 import BGZFStreams: BGZFStream
 import BioCore.Exceptions: MissingFieldException
-import BioCore.Testing.get_bio_fmt_specimens
 import BioSequences: @dna_str, @aa_str
-import YAML
+
 
 import BioCore:
     header,
@@ -32,7 +32,7 @@ function randrange(range)
 end
 
 @testset "SAM" begin
-    samdir = joinpath(get_bio_fmt_specimens("master", false, true), "SAM")
+    samdir = path_of_format("SAM")
 
     @testset "MetaInfo" begin
         metainfo = SAM.MetaInfo()
@@ -172,8 +172,8 @@ end
             end
             return true
         end
-        for specimen in YAML.load_file(joinpath(samdir, "index.yml"))
-            filepath = joinpath(samdir, specimen["filename"])
+        for specimen in list_valid_specimens("SAM")
+            filepath = joinpath(samdir, filename(specimen))
             mktemp() do path, io
                 # copy
                 reader = open(SAM.Reader, filepath)
@@ -192,7 +192,7 @@ end
 end
 
 @testset "BAM" begin
-    bamdir = joinpath(get_bio_fmt_specimens("master", false), "BAM")
+    bamdir = path_of_format("BAM")
 
     @testset "AuxData" begin
         auxdata = BAM.AuxData(UInt8[])
@@ -370,11 +370,11 @@ end
     end
 
     @testset "Round trip" begin
-        for specimen in YAML.load_file(joinpath(bamdir, "index.yml"))
-            filepath = joinpath(bamdir, specimen["filename"])
+        for specimen in list_valid_specimens("BAM")
+            filepath = joinpath(bamdir, filename(specimen))
             mktemp() do path, _
                 # copy
-                if occursin("bai", get(specimen, "tags", ""))
+                if hastags(specimen) && in("bai", tags(specimen))
                     reader = open(BAM.Reader, filepath, index=filepath * ".bai")
                 else
                     reader = open(BAM.Reader, filepath)
